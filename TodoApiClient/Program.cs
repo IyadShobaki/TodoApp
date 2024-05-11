6a1cf55a40a9f2c40cd47be74a833bb46421ee40
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-11 04:33:42 -0400

    Added HttpClient Config

--- TodoApiClient/Program.cs
+++ TodoApiClient/Program.cs
@@ -3,7 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddHttpClient("api", opts =>
+{
+   opts.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ApiUrl"));
+});
 
 var app = builder.Build();
 
