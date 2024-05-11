062be73cda27d2f727187ce5a34362b8b760f372
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-11 14:35:22 -0400

    Created Login Page

---
+++ TodoApiClient/Models/TodoUIModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApiClient.Models;
+
+public class TodoUIModel
+{
+    public int Id { get; set; }
+    public string? Task { get; set; }
+    public int AssignedTo { get; set; }
+    public bool IsComplete { get; set; }
+}
