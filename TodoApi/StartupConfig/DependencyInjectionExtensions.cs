74450bf394dc8c86cb65a5e78d8e2a0568b825e3
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-12 02:50:32 -0400

    Enable Authentication InSwagger - Added configuration to setup the API to accept bearer token in the header

--- TodoApi/StartupConfig/DependencyInjectionExtensions.cs
+++ TodoApi/StartupConfig/DependencyInjectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using TodoLibrary.DataAccess;
 
@@ -7,50 +8,83 @@ namespace TodoApi.StartupConfig;
 
 public static class DependencyInjectionExtensions
 {
-   public static void AddStandardServices(this WebApplicationBuilder builder)
-   {
-      builder.Services.AddControllers();
-      builder.Services.AddEndpointsApiExplorer();
-      builder.Services.AddSwaggerGen();
-   }
+    public static void AddStandardServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.AddSwaggerServices();
+    }
 
-   public static void AddCustomServices(this WebApplicationBuilder builder)
-   {
-      builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-      builder.Services.AddSingleton<ITodoData, TodoData>();
-   }
+    private static void AddSwaggerServices(this WebApplicationBuilder builder)
+    {
+        var securityScheme = new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Description = "JWT Authorization header info using bearer tokens",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        };
 
-
-   public static void AddAuthServices(this WebApplicationBuilder builder)
-   {
-      builder.Services.AddAuthorization(opts =>
-      {
-         opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-             .RequireAuthenticatedUser()
-             .Build();
-      });
-
-
-      builder.Services.AddAuthentication("Bearer")
-         .AddJwtBearer(opts =>
-         {
-            opts.TokenValidationParameters = new()
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
             {
-               ValidateIssuer = true,
-               ValidateAudience = true,
-               ValidateIssuerSigningKey = true,
-               ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
-               ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
-               IssuerSigningKey = new SymmetricSecurityKey(
-               Encoding.ASCII.GetBytes(
-                   builder.Configuration.GetValue<string>("Authentication:SecretKey")))
-            };
-         });
-   }
-   public static void AddHealthCheckServices(this WebApplicationBuilder builder)
-   {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type= ReferenceType.SecurityScheme,
+                        Id = "bearerAuth"
+                    }
+                },
+                new string[] {}
+            }
+        };
+        builder.Services.AddSwaggerGen(opts =>
+        {
+            opts.AddSecurityDefinition("bearerAuth", securityScheme);
+            opts.AddSecurityRequirement(securityRequirement);
+        });
+    }
 
-      builder.Services.AddHealthChecks()
-            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
-   }
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<ITodoData, TodoData>();
+    }
+
+
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.FallbackPolicy = new AuthorizationPolicyBuilder()
+              .RequireAuthenticatedUser()
+              .Build();
+        });
+
+
+        builder.Services.AddAuthentication("Bearer")
+           .AddJwtBearer(opts =>
+           {
+               opts.TokenValidationParameters = new()
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                   ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                   IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(
+                    builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+               };
+           });
+    }
+    public static void AddHealthCheckServices(this WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddHealthChecks()
+              .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+    }
 }
