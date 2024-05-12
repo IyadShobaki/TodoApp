fff3c5f2f28a62cbde0be3143eb78bdbc470e91e
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-12 00:48:19 -0400

    Added Generated API Client  - Added Blazor app - Right click on connected services - Add new OpneAPI service using a url

---
+++ TodoApiGeneratedClient/Models/AuthenticationModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApiGeneratedClient.Models;
+
+public class AuthenticationModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ TodoApiGeneratedClient/Models/TokenModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TodoApiGeneratedClient.Models;
+
+public class TokenModel
+{
+    public string Token { get; set; }
+}
