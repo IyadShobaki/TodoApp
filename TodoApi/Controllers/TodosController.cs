6f1b1c1f5c496b088c7c42a533c4e26f8926db86
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-12 20:25:33 -0400

    Practicing Get More Request Info  - Added custom middlware to log info before and after endpoint called - Practicing getting query and uri param from a URL

--- TodoApi/Controllers/TodosController.cs
+++ TodoApi/Controllers/TodosController.cs
@@ -9,152 +9,152 @@ namespace TodoApi.Controllers;
 [ApiController]
 public class TodosController : ControllerBase
 {
-   private readonly ITodoData _data;
-   private readonly ILogger<TodosController> _logger;
+    private readonly ITodoData _data;
+    private readonly ILogger<TodosController> _logger;
 
-   public TodosController(ITodoData data, ILogger<TodosController> logger)
-   {
-      _data = data;
-      _logger = logger;
-   }
+    public TodosController(ITodoData data, ILogger<TodosController> logger)
+    {
+        _data = data;
+        _logger = logger;
+    }
 
-   private int GetUserId()
-   {
-      // Built in functionality (Part of ASP.NET Core) to get the claim "Sub == NameIdentifier".
-      // We tried to put it in ctor because each user will create a new instance of this class
-      // when using an endpoint (Transient). But at the time of creating a new intance Claims/Token
-      // Will not be create it yet and the value will be null
-      var userIdText = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
-      return int.Parse(userIdText);
-   }
-   // GET: api/Todos
-   [HttpGet]
-   // Using ActionResult to be able to return the right status code
-   // Its more flexible than just returning only IEnumerable<TodoDbModel>
-   // which will return only ok status code no matter what.
-   // We use ActionResult and not the interface IActionResult
-   // becuase its allow us passing strongly typed object
-   public async Task<ActionResult<List<TodoDbModel>>> Get()
-   {
-      _logger.LogInformation("GET: api/Todos");
+    private int GetUserId()
+    {
+        // Built in functionality (Part of ASP.NET Core) to get the claim "Sub == NameIdentifier".
+        // We tried to put it in ctor because each user will create a new instance of this class
+        // when using an endpoint (Transient). But at the time of creating a new intance Claims/Token
+        // Will not be create it yet and the value will be null
+        var userIdText = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        return int.Parse(userIdText);
+    }
+    // GET: api/Todos
+    [HttpGet]
+    // Using ActionResult to be able to return the right status code
+    // Its more flexible than just returning only IEnumerable<TodoDbModel>
+    // which will return only ok status code no matter what.
+    // We use ActionResult and not the interface IActionResult
+    // becuase its allow us passing strongly typed object
+    public async Task<ActionResult<List<TodoDbModel>>> Get()
+    {
+        //_logger.LogInformation("GET: api/Todos");
 
-      try
-      {
-         var output = await _data.GetAllAssigned(GetUserId());
-         return Ok(output);
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The GET call to api/Todos failed.");
-         return BadRequest();
-      }
-
-
-   }
-
-   // GET api/Todos/5
-   [HttpGet("{todoId}")]
-   public async Task<ActionResult<TodoDbModel>> Get(int todoId)
-   {
-      _logger.LogInformation("GET: api/Todos/{TodoId}", todoId);
-      try
-      {
-         var output = await _data.GetOneAssigned(GetUserId(), todoId);
-
-         return Ok(output);
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The GET call to {ApiPath} failed. The Id was {TodoId}",
-                     "api/Todos/TodoId", todoId);
-         // we can use string interoplation $ with args but not with the message 
-         //_logger.LogError(ex, "The GET call to {ApiPath} failed. The Id was {TodoId}",
-         //            $"api/Todos/{TodoId}", todoId);
-
-         return BadRequest();
-      }
-   }
-
-   // POST api/Todos
-   [HttpPost]
-   public async Task<ActionResult<TodoDbModel>> Post([FromBody] string task)
-   {
+        try
+        {
+            var output = await _data.GetAllAssigned(GetUserId());
+            return Ok(output);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The GET call to api/Todos failed.");
+            return BadRequest();
+        }
 
 
-      _logger.LogInformation("POST: api/Todos (Task: {Task})", task);
+    }
 
-      try
-      {
-         var output = await _data.Create(GetUserId(), task);
-         return Ok(output);
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The POST call to api/Todos failed. Task value was {Task}", task);
-         return BadRequest();
-      }
+    // GET api/Todos/5
+    [HttpGet("{todoId}")]
+    public async Task<ActionResult<TodoDbModel>> Get(int todoId)
+    {
+        //_logger.LogInformation("GET: api/Todos/{TodoId}", todoId);
+        try
+        {
+            var output = await _data.GetOneAssigned(GetUserId(), todoId);
 
-   }
+            return Ok(output);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The GET call to {ApiPath} failed. The Id was {TodoId}",
+            "api/Todos/TodoId", todoId);
+            // we can use string interoplation $ with args but not with the message 
+            //_logger.LogError(ex, "The GET call to {ApiPath} failed. The Id was {TodoId}",
+            //            $"api/Todos/{TodoId}", todoId);
 
-   // PUT api/Todos/5
-   [HttpPut("{todoId}")]
-   public async Task<IActionResult> Put(int todoId, [FromBody] string task)
-   {
-      _logger.LogInformation("PUT: api/Todos/{TodoId} (Task: {Task})", todoId, task);
+            return BadRequest();
+        }
+    }
 
-      try
-      {
-         await _data.UpdateTask(GetUserId(), todoId, task);
-         return Ok();
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The PUT call to api/Todos/{TodoId} failed. Task value was {Task}",
-            todoId, task);
-         return BadRequest();
-      }
-
-   }
-
-   // PUT api/Todos/5/Complete
-   [HttpPut("{todoId}/Complete")]
-   public async Task<IActionResult> Complete(int todoId)
-   {
-
-      _logger.LogInformation("PUT: api/Todos/{TodoId}/Complete", todoId);
-
-      try
-      {
-         await _data.CompleteTodo(GetUserId(), todoId);
-         return Ok();
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The PUT call to api/Todos/{TodoId}/Complete failed.",
-            todoId);
-         return BadRequest();
-      }
-
-   }
-
-   // DELETE api/Todos/5
-   [HttpDelete("{todoId}")]
-   public async Task<IActionResult> Delete(int todoId)
-   {
+    // POST api/Todos
+    [HttpPost]
+    public async Task<ActionResult<TodoDbModel>> Post([FromBody] string task)
+    {
 
 
+        // _logger.LogInformation("POST: api/Todos (Task: {Task})", task);
 
-      _logger.LogInformation("DELETE: api/Todos/{TodoId}", todoId);
+        try
+        {
+            var output = await _data.Create(GetUserId(), task);
+            return Ok(output);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The POST call to api/Todos failed. Task value was {Task}", task);
+            return BadRequest();
+        }
 
-      try
-      {
-         await _data.Delete(GetUserId(), todoId);
-         return Ok();
-      }
-      catch (Exception ex)
-      {
-         _logger.LogError(ex, "The DELETE call to api/Todos/{TodoId} failed.", todoId);
-         return BadRequest();
-      }
-   }
+    }
+
+    // PUT api/Todos/5
+    [HttpPut("{todoId}")]
+    public async Task<IActionResult> Put(int todoId, [FromBody] string task)
+    {
+        //_logger.LogInformation("PUT: api/Todos/{TodoId} (Task: {Task})", todoId, task);
+
+        try
+        {
+            await _data.UpdateTask(GetUserId(), todoId, task);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The PUT call to api/Todos/{TodoId} failed. Task value was {Task}",
+               todoId, task);
+            return BadRequest();
+        }
+
+    }
+
+    // PUT api/Todos/5/Complete
+    [HttpPut("{todoId}/Complete")]
+    public async Task<IActionResult> Complete(int todoId)
+    {
+
+        //_logger.LogInformation("PUT: api/Todos/{TodoId}/Complete", todoId);
+
+        try
+        {
+            await _data.CompleteTodo(GetUserId(), todoId);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The PUT call to api/Todos/{TodoId}/Complete failed.",
+               todoId);
+            return BadRequest();
+        }
+
+    }
+
+    // DELETE api/Todos/5
+    [HttpDelete("{todoId}")]
+    public async Task<IActionResult> Delete(int todoId)
+    {
+
+
+
+        // _logger.LogInformation("DELETE: api/Todos/{TodoId}", todoId);
+
+        try
+        {
+            await _data.Delete(GetUserId(), todoId);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "The DELETE call to api/Todos/{TodoId} failed.", todoId);
+            return BadRequest();
+        }
+    }
 }
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Web;
 using TodoApi.StartupConfig;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,20 +11,87 @@ builder.AddAuthServices();
 builder.AddHealthCheckServices();
 builder.AddCustomServices();
 
+
 var app = builder.Build();
+
+
+// Example of routing a request (Youtube tutorial)   
+// Endpoint task/4  - (.*) will take whatever after the slash and added to it to the url
+// $1 will tak whatever in (.*)
+
+/*app.UseRewriter(new Microsoft.AspNetCore.Rewrite.RewriteOptions()
+        .AddRedirect("tasks/(.*)", "todos/$1"));*/   // It didn't work
+
+/*var options = new Microsoft.AspNetCore.Rewrite.RewriteOptions()
+        .AddRewrite("tasks/(.*)", "todos/$1", true);
+
+app.UseRewriter(options);*/
+
+/*app.Use(async (conext, next) =>
+{
+    var url = conext.Request.Path.Value;
+
+    string[] pathValue = url.Split("/");
+    var uriParam = pathValue[pathValue.Length - 1];
+
+    Regex regex = new Regex(@"^\d$");
+
+    if (url!.Contains("tasks") && regex.IsMatch(uriParam))
+    {
+        conext.Request.Path = $"/api/todos/{uriParam}";
+    }
+
+    await next();
+});*/
+
+
 
 if (app.Environment.IsDevelopment())
 {
-   app.UseSwagger();
-   app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+
+
+// Adding custom middlware to log the information before and after each endpoint request (Youtube tutorial)
+app.Use(async (context, next) =>
+{
+
+    var displayURL = context.Request.HttpContext.Request.GetDisplayUrl();
+    var parsedUrl = displayURL.Split('?')[1];
+
+    var paramsCollection = HttpUtility.ParseQueryString(parsedUrl);
+
+    foreach (var key in paramsCollection.AllKeys)
+    {
+        Console.WriteLine($"Key: {key} => Value: {paramsCollection[key]}");
+    }
+
+    string[] uriPath = context.Request.Path.Value.TrimStart('/').Split('/');
+    var uriParm = uriPath[uriPath.Length - 1];
+    //var typedHEader = context.Request.HttpContext.Request.GetTypedHeaders();
+    //var headersValues = context.Request.HttpContext.Request.Headers.Values;
+    //var bodyType = context.Request.HttpContext.Request.Body.GetType;
+    //var routeValue = context.Request.HttpContext.GetRouteValue;
+    string testContext = $"[{context.Request.Method} {context.Request.Path.Value} {DateTime.UtcNow}] Started.";
+    //Console.WriteLine(testContext);
+    app.Logger.LogInformation(testContext);
+    await next(context);
+    string testContextNext = $"[{context.Request.Method} {context.Request.Path.Value} {DateTime.UtcNow}] Finished.";
+    //Console.WriteLine(testContextNext);
+    app.Logger.LogInformation(testContextNext);
+
+    //var testContextNext1 = context.Response.Headers.ToList();
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHealthChecks("/health").AllowAnonymous();
+
 
 app.Run();
